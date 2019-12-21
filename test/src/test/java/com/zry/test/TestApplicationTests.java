32a0243d842deea64ef5a8fc76dd585364bfe3d8
赵若愚 <30320133+zry279@users.noreply.github.com>
2019-12-21 14:07:21 +0800

    第一次测试提交 from IDEA

---
+++ test/src/test/java/com/zry/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zry.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
